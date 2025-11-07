57be42cb7eccaf8c1ebe177664ba122a38a7afac
Nathan Coquerel <Nathan.Coquerel@univ-grenoble-alpes.fr>
2025-11-07 11:56:38 +0100

    🐛 Fix chained parsing not using structure specification

--- src/lib/cnl/parser.ts
+++ src/lib/cnl/parser.ts
@@ -1,21 +1,31 @@
 import nearley from 'nearley';
 
-import rules from './cnl_tactic_specifier';
+import rules, { type StructureSpecification } from './cnl_tactic_specifier';
 import tactic_grammar, { type ParseResult } from './cnl_grammar';
 
 const { Grammar, Parser } = nearley;
 
 export const grammar = Grammar.fromCompiled(rules);
 
-export function parse_proposition(
-	value: string,
-	state?: string[]
-): { offset: number; result: ParseResult; state: string[] } | undefined {
+export type CNLParseResult = {
+	offset: number;
+	result: ParseResult;
+	state: string[];
+};
+
+export type CNLParseResultChained = {
+	offset: number;
+	result: ParseResult[];
+	state: string[];
+	action?: StructureSpecification['specification'];
+};
+
+export function parse_cnl(value: string, state?: string[]): CNLParseResult | undefined {
 	const compiled_rules = tactic_grammar(undefined, state);
 	const grammar = Grammar.fromCompiled(compiled_rules);
 	const parser = new Parser(grammar);
 
-	let max: { offset: number; result: ParseResult; state: string[] } | undefined = undefined;
+	let max: CNLParseResult | undefined = undefined;
 	for (let i = 0; i < value.length; i++) {
 		try {
 			parser.feed(value[i]);
@@ -36,23 +46,24 @@ export function parse_proposition(
 	return max;
 }
 
-export function parse_proposition_chained(value: string, state: string[] = []) {
+export function parse_cnl_chained(value: string, state: string[] = []): CNLParseResultChained {
 	let _state = [...state];
 	let _offset = 0;
 
-	let result: ReturnType<typeof parse_proposition> = undefined;
+	let result: ReturnType<typeof parse_cnl> = undefined;
 	let parsed: ParseResult[] = [];
 	do {
-		result = parse_proposition(value.substring(_offset), _state);
+		result = parse_cnl(value.substring(_offset), _state);
 		if (!result) continue;
 		_offset += result.offset;
 		_state = result.state;
 		parsed.push(result.result);
-	} while (result);
+	} while (result && !result.result.tactic.spec.footer.structure);
 
 	return {
 		result: parsed,
 		state: _state,
-		offset: _offset
+		offset: _offset,
+		action: parsed[parsed.length - 1]?.tactic?.spec?.footer?.structure?.specification
 	};
 }
