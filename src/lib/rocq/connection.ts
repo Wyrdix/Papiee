62b5c01bd4355d9acddab4d6c83903366a39f297
Nathan Coquerel <Nathan.Coquerel@univ-grenoble-alpes.fr>
2025-11-13 11:53:28 +0100

    🚧 Fix some problems with the worker workspace

--- src/lib/rocq/connection.ts
+++ src/lib/rocq/connection.ts
@@ -4,7 +4,7 @@ import * as types from 'vscode-languageserver-types';
 
 import * as raw from '$lib/../../rocq/rocq.json';
 
-const rocqDumpFile = raw satisfies RocqDumpFile;
+const rocqDumpFile = raw as RocqDumpFile;
 
 type RocqDumpFile = {
 	files: { [filename in string]: string };
@@ -28,12 +28,10 @@ export async function initialize(
 	connection: proto.MessageConnection,
 	params: Partial<proto.InitializeParams> = {}
 ) {
-	connection.onNotification(proto.LogMessageNotification.type, console.log);
 	let initializeParameters: proto.InitializeParams = {
 		...params,
 		processId: null,
-		rootUri: 'file:///exercise/',
-		workspaceFolders: null,
+		rootUri: 'file://exercise',
 		initializationOptions: { eager_diagnostics: true, messages_follow_goal: true },
 		trace: 'verbose',
 		capabilities: {
@@ -45,56 +43,65 @@ export async function initialize(
 		}
 	};
 
+	connection.onNotification(proto.LogMessageNotification.type, (msg) => {
+		console.log('LSP log:', msg);
+	});
+	connection.onNotification(proto.ShowMessageNotification.type, (msg) => {
+		console.log('LSP show:', msg);
+	});
+
 	await connection.sendNotification(proto.SetTraceNotification.type, { value: 'verbose' });
-
 	await connection.sendRequest(proto.InitializeRequest.type, initializeParameters);
-
+	await connection.sendNotification(proto.InitializedNotification.type, {});
 	await Promise.all(
 		Object.keys(rocqDumpFile.files).map(async (path) => {
-			let uri = 'file:///exercise/' + path;
-			let languageId = 'rocq';
-			let version = 1;
-			let text = (rocqDumpFile as any).files[path];
-			let textDocument = types.TextDocumentItem.create(uri, languageId, version, text);
-			let openParams: proto.DidOpenTextDocumentParams = { textDocument };
-			await connection
-				.sendNotification(proto.DidOpenTextDocumentNotification.type, openParams)
-				.then(console.log)
-				.catch(console.error);
+			const uri = 'file://exercise/' + path;
+			const languageId = 'rocq';
+			const version = 1;
+			const text = rocqDumpFile.files[path];
+			const textDocument = types.TextDocumentItem.create(uri, languageId, version, text);
+			const openParams: proto.DidOpenTextDocumentParams = { textDocument };
 
-			await connection
-				.sendNotification('coq/workspace_update')
-				.then(console.log)
-				.catch(console.error);
-			await connection.sendRequest('coq/saveVo', openParams).then(console.log).catch(console.error);
+			await connection.sendNotification(proto.DidOpenTextDocumentNotification.type, openParams);
 		})
 	);
 
-	await connection.sendNotification('coq/workspace_update').then(console.log).catch(console.error);
-	{
-		const path = 'main.v';
-		const value = 'Require Import Test.\nPrint Lemma.';
-		let uri = 'file:///exercise/' + path;
-		let languageId = 'rocq';
-		let version = 1;
-		let text = value;
-		let textDocument = types.TextDocumentItem.create(uri, languageId, version, text);
-		let openParams: proto.DidOpenTextDocumentParams = { textDocument };
-		await connection
-			.sendNotification(proto.DidOpenTextDocumentNotification.type, openParams)
-			.then(console.log)
-			.catch(console.error);
+	await connection.sendNotification('coq/workspace_update', {
+		added: Object.keys(rocqDumpFile.files).map((path) => ({
+			uri: `file://exercise/${path}`
+		})),
+		removed: []
+	});
 
-		await connection
-			.sendRequest('proof/goals', {
-				textDocument: { uri, version } satisfies proto.VersionedTextDocumentIdentifier,
-				position: { line: 0, character: 10 } satisfies proto.Position,
-				pp_format: 'String',
-				mode: 'After'
-			})
-			.then(console.log)
-			.catch(console.error);
-	}
+	await Promise.all(
+		Object.keys(rocqDumpFile.files).map(async (path) => {
+			const uri = `file://exercise/${path}`;
+			const textDocument = types.TextDocumentItem.create(uri, 'rocq', 1, rocqDumpFile.files[path]);
+			const openParams: proto.DidOpenTextDocumentParams = { textDocument };
+
+			await connection.sendRequest('coq/saveVo', openParams);
+		})
+	);
+
+	const path = 'main.v';
+	const value = 'Require Import Test.\nPrint Lemma.';
+	let uri = 'file://exercise/' + path;
+	let languageId = 'rocq';
+	let version = 1;
+	let text = value;
+	let textDocument = types.TextDocumentItem.create(uri, languageId, version, text);
+	let openParams: proto.DidOpenTextDocumentParams = { textDocument };
+	await connection.sendNotification(proto.DidOpenTextDocumentNotification.type, openParams);
+
+	await connection
+		.sendRequest('proof/goals', {
+			textDocument: { uri, version } satisfies proto.VersionedTextDocumentIdentifier,
+			position: { line: 0, character: 10 } satisfies proto.Position,
+			pp_format: 'String',
+			mode: 'After'
+		})
+		.then(console.log)
+		.catch(console.error);
 
 	return connection;
 }
